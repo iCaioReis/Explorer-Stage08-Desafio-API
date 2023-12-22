6bb8fc26f8ac917a40fa0cf5e1e1ad6d1d6f92a9
iCaioReis <caioreis3186@gmail.com>
2023-12-21 21:28:42 -0300

    Add AppError

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("express-async-errors");
 
+const AppError = require("./utils/AppError,js");
 
 const express = require ('express');
 const routes = require ("./routes");
@@ -9,6 +10,21 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(( error, req, res, next) => {
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+    console.error(error);
+
+    return res.status(500).json({
+        status: "error",
+        message: "internal server error"
+    });
+});
 
 const PORT = 3333;
 
